d7e338fbbda0c6e6646d918c57ef3504c0b2d0ca
Lucas Garin <lucas.garin73@gmail.com>
2018-01-04 21:13:32 +0100

    remove row in view dynamically

--- front/src/app/users/users.component.ts
+++ front/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../user/user.service";
 import * as _ from "lodash";
 import {discardPeriodicTasks} from "@angular/core/testing";
@@ -6,28 +6,36 @@ import {ConfirmModal, ModalComponent} from "../modal/modal.component";
 import {SuiModalService} from "ng2-semantic-ui";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-users;
-  constructor(private userServ: UserService, private modalService: SuiModalService) { }
+    users;
 
-  ngOnInit() {
-    this.userServ.getAll().subscribe(data=>this.users = data);
-  }
+    constructor(private userServ: UserService, private modalService: SuiModalService) {
+    }
 
-  getRole(userRole) {
-     if (_.includes(userRole, 'ROLE_ADMIN')) return "Administrateur";
-     return "Utilisateur";
-  }
+    ngOnInit() {
+        this.userServ.getAll().subscribe(data => this.users = data);
+    }
 
-  delete(id:string) {
-      console.log(id);
-      this.modalService
-          .open(new ConfirmModal("Suppression", "Êtes vous sûr de vouloir supprimer cet utilisateur ? Vous ne pourrez pas revenir en arrière."))
-          .onApprove(() => this.userServ.delete(id).subscribe(() => location.reload()));
-  }
+    getRole(userRole) {
+        if (_.includes(userRole, 'ROLE_ADMIN')) return "Administrateur";
+        return "Utilisateur";
+    }
+
+    delete(id: string) {
+        console.log(this.users);
+        this.modalService
+            .open(new ConfirmModal("Suppression", "Êtes vous sûr de vouloir supprimer cet utilisateur ? Vous ne pourrez pas revenir en arrière."))
+            .onApprove(() => this.userServ.delete(id).subscribe(() => {
+                this.users = _.remove(this.users, function (n) {
+                    return n.id != id;
+                });
+                console.log(this.users);
+                console.log();
+            }));
+    }
 
 }
